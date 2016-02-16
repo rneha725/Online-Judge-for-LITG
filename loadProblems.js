be89a4a12f241de1461b97bc2e3c86c9ea638a91
rneha725 <rneha725@gmail.com>
2016-02-16 23:49:33 +0530

    folder problem added with js files.

--- loadProblems.js
+++ loadProblems.js
@@ -1,22 +1,5 @@
 function fetchProblems()
 {
-	var problemArray=[
-	{
-		code:"problem1",
-		name:"Hello World",
-		level:2
-	},
-	{
-		code:"problem2",
-		name:"Life, Universe and Everything",
-		level:3
-	},
-	{
-		code:"problem3",
-		name:'Factorial',
-		level:4
-	}
-	];
 	var innerProblemList=document.getElementById("problems");
 	var str='';
 	for(i in problemArray)
---
+++ problems.js
@@ -0,0 +1,17 @@
+	var problemArray=[
+	{
+		code:"problem1",
+		name:"Hello World",
+		level:2
+	},
+	{
+		code:"problem2",
+		name:"Life, Universe and Everything",
+		level:3
+	},
+	{
+		code:"problem3",
+		name:'Factorial',
+		level:4
+	}
+	];
