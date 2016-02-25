b799e811cebe4f02065ced8c1d14c66930fd8396
rneha725 <rneha725@gmail.com>
2016-02-26 01:35:46 +0530

    Add CodeMirror API for C/C++/Python

---
+++ js/submit.js
@@ -0,0 +1,18 @@
+var codeMirror;
+function changeSyntax() {
+	if(document.getElementById("c").selected || document.getElementById("c++").selected) {
+		codeMirror.setOption("mode", "cmake");
+		
+	}
+	else if(document.getElementById("python").selected) {
+		codeMirror.setOption("mode", "python");
+	}
+};
+
+window.onload =	function() {
+	codeMirror = CodeMirror.fromTextArea(document.getElementById("codeBox"), {
+	lineNumbers: true,
+	matchBrackets: true
+	});
+//	changeSyntax();
+}
